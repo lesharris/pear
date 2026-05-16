f8566cec7d143f5aa84bcc775e6ca24568df7c38
Les Harris <les@lesharris.com>
2026-05-16 12:52:17 -0700

    feat: enhance PModal functionality to support promise-based open and close actions

--- packages/pear/src/types/PModal.ts
+++ packages/pear/src/types/PModal.ts
@@ -4,7 +4,7 @@ export type PModalAction = {
   label: string
   variant?: 'secondary' | 'contrast'
   outline?: boolean
-  action?: () => void | Promise<void>
+  action?: () => unknown | Promise<unknown>
 }
 
 export type PModalOptions = {
@@ -17,8 +17,8 @@ export type PModalOptions = {
 }
 
 export type PModalContext = {
-  open: (options: PModalOptions) => void
-  close: () => void
+  open: (options: PModalOptions) => Promise<unknown>
+  close: (result?: unknown) => void
 }
 
 export const PModalKey: InjectionKey<PModalContext> = Symbol('PModal')
